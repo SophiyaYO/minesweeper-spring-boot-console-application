2cc508b7c58d30b849c4e11fee698282981258c3
Sofia Yo <dimitrova.sophiya@gmail.com>
2020-09-01 14:17:48 +0300

    initial commit

---
+++ src/test/java/com/minesweeper/demo/BoardControllerTests.java
@@ -0,0 +1,12 @@
+package com.minesweeper.demo;
+
+import com.minesweeper.demo.controller.BoardController;
+
+public class BoardControllerTests {
+    //test with Beginner level (0)
+    BoardController testGameBoardFirst = new BoardController(0);
+    //test with Intermediate level (1)
+    BoardController testGameBoardSecond = new BoardController(1);
+
+
+}
